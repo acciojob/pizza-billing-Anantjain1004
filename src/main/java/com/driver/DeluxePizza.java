7ce0651338a19f411830263c63e601291aa6c29e
Anantjain1004 <jainanant895@gmail.com>
2023-01-17 12:39:41 +0530

    Solution 1

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -2,8 +2,20 @@ package com.driver;
 
 public class DeluxePizza extends Pizza {
 
+    private int price;
+    private String bill;
+
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
-        // your code goes here
+        super.addExtraCheese();
+        super.addExtraToppings();
+    }
+
+    @Override
+    public void addExtraCheese() {
+    }
+
+    @Override
+    public void addExtraToppings() {
     }
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -6,9 +6,28 @@ public class Pizza {
     private Boolean isVeg;
     private String bill;
 
+    private int extraCheesePrice = 80;
+    private int extraToppingPrice = 70;
+    private int extraNonVegToppingPrice = 120;
+    private int paperBag = 20;
+    private int basePizzaPrice;
+
+    private boolean isExtraCheeseAdded = false;
+    private boolean isExtraToppingAdded = false;
+    private boolean isOptedForTakeAway = false;
+
+
+
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        if(isVeg == true) {
+            this.price = 300;
+        }
+        else {
+            this.price = 400;
+        }
+        basePizzaPrice = this.price;
     }
 
     public int getPrice(){
@@ -16,19 +35,39 @@ public class Pizza {
     }
 
     public void addExtraCheese(){
-        // your code goes here
+        isExtraCheeseAdded = true;
+        this.price += extraCheesePrice;
     }
 
     public void addExtraToppings(){
-        // your code goes here
+        isExtraToppingAdded = true;
+        if(isVeg == true){
+            this.price += extraToppingPrice;
+        }
+        else{
+            this.price += extraNonVegToppingPrice;
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        isOptedForTakeAway = true;
+        this.price += paperBag;
     }
 
     public String getBill(){
-        // your code goes here
-        return this.bill;
+        String bill = "";
+        System.out.println("Base Price Of The Pizza: " + basePizzaPrice);
+        if(isExtraCheeseAdded) {
+            bill += "Extra Cheese Added: " + extraCheesePrice + "\n";
+        }
+        if(isExtraToppingAdded) {
+            bill +=  "Extra Toppings Added: " + extraToppingPrice + "\n";
+        }
+        if(isOptedForTakeAway) {
+            bill += "Paperbag Added: " + paperBag + "\n";
+        }
+        bill += "Total Price: " + this.price + "\n";
+        return bill;
     }
 }
